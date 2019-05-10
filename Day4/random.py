0d469a1f10a9d75f4d4baa1cef75dea4dc0f55a5
Yuta Lin <linyuta1230@gmail.com>
2019-05-10 16:19:50 +0800

    add random.py for learning while statement

---
+++ Day4/random.py
@@ -0,0 +1,17 @@
+import random
+
+answer = random.randint(1, 100)
+counter = 0
+while True:
+    counter += 1
+    number = int(input('Please enter: '))
+    if number < answer:
+        print('bigger')
+    elif number > answer:
+        print('smaller')
+    else:
+        print('Bingo!')
+        break
+print('Total guess time:%d次' % counter)
+if counter > 7:
+    print('You need to improve your guessing skill')
