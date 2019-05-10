976f111295ab402a0a0d470804ad80b62047188c
Yuta Lin <linyuta1230@gmail.com>
2019-05-10 16:00:25 +0800

    add triangle.py

---
+++ Day3/triangle.py
@@ -0,0 +1,12 @@
+import math
+
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    print('perimeter: %f' % (a + b + c))
+    p = (a + b + c) / 2
+    area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+    print('area: %f' % (area))
+else:
+    print("It's not a trangle")
