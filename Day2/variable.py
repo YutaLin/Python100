d5c110f2f65244548b8c39bada95dda346a3fb57
Yuta Lin <linyuta1230@gmail.com>
2019-05-10 14:19:36 +0800

    Use variable to store data and do arithmetic

---
+++ Day2/variable.py
@@ -0,0 +1,17 @@
+"""
+Use variable to store data and do arithmetic
+
+version: 0.1
+author: Yuta
+"""
+
+a = 321
+b = 123
+
+print("a+b =", a + b)
+print("a-b =", a - b)
+print("a*b =", a * b)
+print("a/b =", a / b)
+print("a//b =", a // b) #divde by no reminder
+print("a%b=", a % b)
+print("a**b=", a ** b) #power
